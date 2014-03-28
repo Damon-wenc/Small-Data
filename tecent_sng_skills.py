995ab0ff413218e5a0439c701ed90972628a53d3
Damon-wenc <mnn.fantasy@gmail.com>
2014-03-29 00:42:30 +0800

    adding keywords in progress

--- tecent_sng_skills.py
+++ tecent_sng_skills.py
@@ -3,8 +3,16 @@
 import re
 import urllib2
 
-html = urllib2.urlopen('http://hr.tencent.com/position_detail.php?id=15013&keywords=SNG%20%E5%90%8E%E5%8F%B0&tid=0&lid=2218').read()
-r = re.compile(r'熟练掌握(.+?)编程语言')
+skills = {
+    '分布式' : 0,
+    '云计算' : 0,
+    '互联网' : 0,
+    '大容量' : 0,
+    'C'      : 0,
+    'C++'    : 0
+    }
 
-for x in r.findall(html):
-    print x
+html = urllib2.urlopen('http://hr.tencent.com/position_detail.php?id=15013&keywords=SNG%20%E5%90%8E%E5%8F%B0&tid=0&lid=2218').read()
+
+print html
+
