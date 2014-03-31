80a1f4bd41fb2d9cfe81f4ba894651c4ef1ab398
Damon-wenc <mnn.fantasy@gmail.com>
2014-03-31 17:00:36 +0800

    added a ordered dict

--- tecent_sng_skills.py
+++ tecent_sng_skills.py
@@ -2,75 +2,37 @@
 
 import re
 import urllib2
+from collections import OrderedDict
 
-skills = {
-    'C、'    : 0,
-    'C/'     : 0,
-    'c++'    : 0,
-    'mysql'  : 0,
-    'tcp/ip' : 0,
-    'cdn'    : 0,
-    'linux'   : 0,
-    'javascript' : 0,
-    'html'    : 0,
-    'nosql'   : 0,
-    'hadoop'  : 0,
-    'spark'   : 0,
-    'storm'   : 0,
-    'lbs'     : 0,
-    '经验'    : 0,
-    '架构'    : 0,
-    '容灾'    : 0,
-    '优化'    : 0,
-    '重构'    : 0,
-    '稳定'    : 0,
-    '沟通'    : 0,
-    '压力'    : 0,
-    '主动'    : 0,
-    '好学'    : 0,
-    '分布式'  : 0,
-    '云计算'  : 0,
-    '互联网'  : 0, 
-    '数据库'  : 0,   
-    '流媒体'  : 0,
-    '富媒体'  : 0,
-    '高性能'  : 0,
-    '高可靠'  : 0,    
-    '高并发'  : 0,
-    '大数据'  : 0,
-    '海量数据': 0,
-    '网络处理': 0,
-    '存储架构': 0
-    '数据处理': 0,
-    '精通C++' : 0,
-    '安全逻辑': 0,
-    '通信系统': 0,
-    '面向对象': 0,
-    '操作系统': 0,
-    '软件工程': 0,
-    '设计模式': 0,
-    '数据结构': 0,
-    '网络安全': 0,
-    '安全漏洞': 0,
-    '网络攻击': 0,
-    '网络开发': 0,
-    '数据分析': 0,
-    '逻辑思维': 0,
-    '实时计算': 0,
-    '实时推荐': 0,
-    '分布式计算' : 0,
-    '大容量网络' : 0,
-    '大容量通信' : 0,
-    '进程间通讯' : 0,
-    '后台业务开发' : 0,
-    '计算机基础结构' : 0,
-    '互联网应用协议' : 0
-     }
+skill_heat = OrderedDict()
+skills_array = ['c、', 'c/', 'c++', 'mysql', 'tcp/ip', 'cdn', 'linux', 'javascript', 'html',
+                'nosql', 'hadoop', 'spark', 'storm', 'lbs', '经验', '架构', '容灾', '优化',
+                '重构', '稳定', '沟通', '压力', '主动', '好学', '分布式', '云计算', '互联网',
+                '数据库', '富媒体', '高性能', '高可靠', '高并发', '大数据', '海量数据', '网络处理',
+                '存储架构', '数据处理', '精通c++', '安全逻辑', '通信系统', '面向对象', '操作系统',
+                '软件工程', '设计模式', '数据结构', '网络安全', '安全漏洞', '网络攻击', '网络并发',
+                '数据分析', '逻辑思维', '实时计算', '实时推荐', '分布式计算', '大容量网络',
+                '大容量通信', '进程间通讯', '后台业务开发', '计算机基础结构', '互联网应用协议']
+
+
+for item in skills_array:
+    skill_heat[item] = 0
+
 
 html = urllib2.urlopen('http://hr.tencent.com/position_detail.php?id=15013&keywords=SNG%20%E5%90%8E%E5%8F%B0&tid=0&lid=2218').read()
 
 #print html.lower()
-print type(html)
-print 'debug--------'
-print html.lower().find('c/c++')
+#print type(html)
+#print 'debug--------'
+#print html.lower().find('c/c++')
+for name, address in skill_heat.items():
+    if html.lower().find(name):
+        #print name
+        address += 1
+        print 'debug------address = %d' %address
+
+
+for name, address in skill_heat.items():
+    #print 'skill: %s \t heat: [%d]' %(name.decode('utf-8'), address)
+    print 'skill: %-20s heat: [%d]' %(name.decode('utf-8'), address)
 
