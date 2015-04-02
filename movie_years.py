ac56751e2e4dc3dad173a3e6eaf6b2957e38eac2
wenchang <bigcow88@163.com>
2015-04-02 23:31:17 +0800

    delete useless lines
    errr… delete extra commented lines

--- movie_years.py
+++ movie_years.py
@@ -8,17 +8,11 @@ page = 10
 
 url = 'http://movie.douban.com/top250?start=%d&filter=&format='
 
-#title_re = re.compile(r'>\n                            <span class="title">(.+?)</span>')
-#country_re = re.compile(r'&nbsp;/&nbsp;(.+?)&nbsp;/&nbsp;')
-#year_re = re.compile(r'                            (.+?)&nbsp;/&nbsp;.+?&nbsp;/&nbsp;')
-#rank_re = re.compile(r'rating.+?em>(.+?)</em></span>')
-#info_re = re.compile(r'title">(.+?)</span>.+?title.+?bd.+?br.+?                            (.+?)&nbsp;/&nbsp;(.+?)&nbsp;/&nbsp;.+?rating.+?<em>(.+?)</em></span>', re.DOTALL)
 info_re = re.compile(r'<img alt="(.+?)".+?bd.+?br.+?                            (.+?)&nbsp;/&nbsp;(.+?)&nbsp;/&nbsp;.+?rating.+?<em>(.+?)</em></span>', re.DOTALL)
 movies = []
 for i in range(0, page):
     page_url = url %(i*25)
     html = urllib2.urlopen(page_url).read()
-    #movie = []
     for x in info_re.findall(html):
         tmp = []
         tmp.append(x[0])
@@ -26,17 +20,7 @@ for i in range(0, page):
         tmp.append(x[2])
         tmp.append(x[3])
         movies.append(tmp)
-        #break
-    #for x in title_re.findall(html):
-        #print x
-    #for y in country_re.findall(html):
-        #print y
-  #  for z in year_re.findall(html):
-      #  index += 1
-       # movie1.append(z)
-        #print z
-    #for w in rank_re.findall(html):
-        #print w
+        
 for item in movies:
     print '%s\t\t\t%s\t\t\t%s\t\t\t%s' %(item[0].decode('utf-8'), item[1].decode('utf-8'), item[2].decode('utf-8'), item[3].decode('utf-8'))
 
@@ -47,10 +31,6 @@ for item in movies:
     else:
         trend[item[1]] = 1
 
-#collections.OrderedDict(sorted(trend.items(), key=lambda t:t[1]))
-
-#for name, address in trend.items():
-    #print 'year %s has %d great moives' %(name, address)
 year = 1895
 while year < 2014:
     if str(year) in trend:
