a992a09c4688153dbb730c872acaadb8d35c0b01
wenchang <bigcow88@163.com>
2015-09-29 22:43:13 +0800

    starts to work
    开工了

--- get_xdytt_hd_movie.py
+++ get_xdytt_hd_movie.py
@@ -1,2 +1,13 @@
-# coding: utf8
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
+'''
+这个小程序是为了将一个电影网（小电影天堂）的资源遍历下保存下来，
+大致凭借2个依据：1. 1080p，2. 豆瓣评分6.0以上，
+
+'''
+# http://www.xdytt.com/?item=movie
+# http://www.xdytt.com/page/1?item=movie
+# http://www.xdytt.com/page/2?item=movie
+# http://www.xdytt.com/page/3?item=movie
+# http://www.xdytt.com/page/165?item=movie
