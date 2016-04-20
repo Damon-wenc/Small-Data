e156f94c83321a4a3c73754e885c23ed7344a93d
wenchang <bigcow88@163.com>
2016-04-20 23:21:55 +0800

    更新爬豆瓣的脚本
    规则过期，重新写了下规则。添加些注释。

--- movie_years.py
+++ movie_years.py
@@ -8,9 +8,18 @@ page = 10
 
 url = 'http://movie.douban.com/top250?start=%d&filter=&format='
 
-info_re = re.compile(r'<img alt="(.+?)".+?bd.+?br.+?                            (.+?)&nbsp;/&nbsp;(.+?)&nbsp;/&nbsp;.+?rating.+?<em>(.+?)</em></span>', re.DOTALL)
+# this regulation was failed. douban changed it's web layout.
+#info_re = re.compile(r'<img alt="(.+?)".+?bd.+?br.+?                            (.+?)&nbsp;/&nbsp;(.+?)&nbsp;/&nbsp;.+?rating.+?<em>(.+?)</em></span>', re.DOTALL)
+
+# 4.20.2016 
+info_re = re.compile(r'img alt="(.+?)" src.+?<div class="bd">.+?(\d+)&nbsp;/&nbsp;(.+?)&nbsp;/&nbsp;.+?<span class="rating_num" property="v:average">(.+?)</span>', re.DOTALL)
+
+'''
+parse 10 pages of movie brief introduction
+'''
 movies = []
 for i in range(0, page):
+    print "Please wait..."
     page_url = url %(i*25)
     html = urllib2.urlopen(page_url).read()
     for x in info_re.findall(html):
@@ -20,10 +29,14 @@ for i in range(0, page):
         tmp.append(x[2])
         tmp.append(x[3])
         movies.append(tmp)
-        
-for item in movies:
-    print '%s\t\t\t%s\t\t\t%s\t\t\t%s' %(item[0].decode('utf-8'), item[1].decode('utf-8'), item[2].decode('utf-8'), item[3].decode('utf-8'))
 
+print "%-40s\t\t%-10s\t%-30s\t\t%-10s" %("电影名", "年代", "地区", "评分")
+for item in movies:
+    print '%-40s\t%-10s\t%-30s\t%-10s' %(item[0].decode('utf-8'), item[1].decode('utf-8'), item[2].decode('utf-8'), item[3].decode('utf-8'))
+
+'''
+accounting and print the result
+'''
 trend = {}
 for item in movies:
     if item[1] in trend:
